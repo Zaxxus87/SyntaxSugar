f30cdfb1bd7da6a8f8a6e50cc11e118fcb9a8a85
Jeff Baker <jeffrey1871@gmail.com>
2021-02-26 17:42:41 -0600

    update

--- SyntaxSugarExercise/Program.cs
+++ SyntaxSugarExercise/Program.cs
@@ -6,10 +6,8 @@ namespace SyntaxSugarExercise
     {
         static void Main(string[] args)
         {
-            var answer = 9;
-
-            var response = answer < 9 ? answer + " is less than nine" : answer + " greater than or equal to nine";
-
+            var answer = 4;
+            var response = answer < 9 ?$"{answer} is less than nine" : $"{answer} greater than or equal to nine";
             Console.WriteLine(response); 
         }
     }
